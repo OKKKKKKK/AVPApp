31cb73e9e247d9933e28f1eb36a802ab589b7088
SagarBhave <SagarBhave@BatmanForever.github.com>
2021-06-02 16:24:32 +0200

    Sidebar Menu Changes

--- frontend/src/app/shared/vertical-menu/vertical-menu-routes.config.ts
+++ frontend/src/app/shared/vertical-menu/vertical-menu-routes.config.ts
@@ -3,16 +3,19 @@ import { RouteInfo } from './vertical-menu.metadata';
 //Sidebar menu Routes and data
 export const ROUTES: RouteInfo[] = [
   {
-    path: '/page', title: 'Page', icon: 'ft-home', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+    path: '/page', title: 'Safety Manual', icon: 'ft-home', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
   },
   {
-    path: '/dashboard/dashboard1', title: 'Dashboard', icon: 'ft-home', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+    path: '/dashboard/dashboard2', title: 'Passenger Safety', icon: 'ft-check-circle', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
   },
   {
-    path: '/dashboard/dashboard2', title: 'Dashboard 2', icon: 'ft-home', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+    path: '/dashboard/dashboard1', title: 'Vehicle Health ', icon: 'ft-activity', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
   },
   {
-    path: '', title: 'Menu Levels', icon: 'ft-align-left', class: 'has-sub', badge: '3', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false,
+    path: '/dashboard/dashboard2', title: 'Passenger Comfort', icon: 'ft-sliders', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: []
+  },
+  {
+    path: '', title: 'Menu Levels', icon: 'ft-align-left', class: 'has-sub', badge: '', badgeClass: 'badge badge-pill badge-danger float-right mr-1 mt-1', isExternalLink: false,
     submenu: [
         { path: '/YOUR-ROUTE-PATH', title: 'Second Level', icon: 'ft-arrow-right submenu-icon', class: '', badge: '', badgeClass: '', isExternalLink: false, submenu: [] },
         {
